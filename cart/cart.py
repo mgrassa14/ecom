c19fd1847968bf9fdd76963ded10ee47e8518ed9
mgrassa14 <mgrassa00@gmail.com>
2024-05-24 08:57:22 -0700

    edited quantity into product and cart summary page

--- cart/cart.py
+++ cart/cart.py
@@ -14,13 +14,15 @@ class Cart():
         # make sure cart is available on all pages of site
         self.cart = cart
         
-    def add(self, product):
+    def add(self, product, quantity):
         product_id = str(product.id)
+        product_qty = str(quantity)
         # logic
         if product_id in self.cart:
             pass
         else:
-            self.cart[product_id] = {'price': str(product.price)}
+            # self.cart[product_id] = {'price': str(product.price)}
+            self.cart[product_id] = int(product_qty)
             
         self.session.modified = True
         
@@ -35,3 +37,7 @@ class Cart():
 
 		# Return those looked up products
         return products
+    
+    def get_quants(self):
+        quantities = self.cart
+        return quantities
--- cart/views.py
+++ cart/views.py
@@ -9,9 +9,9 @@ def cart_summary(request):
     # Get the cart
     cart = Cart(request)
     cart_products = cart.get_prods
-    # quantities = cart.get_quants
+    quantities = cart.get_quants
     # totals = cart.cart_total()
-    return render(request, "cart_summary.html", {"cart_products":cart_products})
+    return render(request, "cart_summary.html", {"cart_products":cart_products, "quantities": quantities})
 
 def cart_add(request):
     # Get the cart
@@ -20,13 +20,13 @@ def cart_add(request):
     if request.POST.get('action') == 'post':
         # Get stuff
         product_id = int(request.POST.get('product_id'))
-        # product_qty = int(request.POST.get('product_qty'))
+        product_qty = int(request.POST.get('product_qty'))
 
 		# lookup product in DB
         product = get_object_or_404(Product, id=product_id)
 		
 		# Save to session
-        cart.add(product=product)
+        cart.add(product=product, quantity=product_qty)
 
 		# Get Cart Quantity
         cart_quantity = cart.__len__()
